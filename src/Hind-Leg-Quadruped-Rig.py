d65c0c09b51404347289f7218bb09bcab4ccfcce
Raquel Baca <raquelbaca4002@gmail.com>
2026-01-03 17:52:51 -0600

    Basic Pseudocode

---
+++ src/Hind-Leg-Quadruped-Rig.py
@@ -0,0 +1,9 @@
+# User selects all properly named leg JNTs
+# hip, knee, ankle, hock, toe
+
+# Create paw con + grp
+# footRoll con + grp
+# IKH
+# footRoll heirarchy
+# Knee PV con + grp
+# Cleanup freeze transform + delete history
